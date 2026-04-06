509f2bfffba5117f9e4f0cfd24f617632075375f
Switch-modder <deolrajjyot@gmail.com>
2026-04-06 19:47:19 -0400

    Add wrapper to flatbuffers for c++17 compat

---
+++ deps/flatbuffers/include/flatbuffers/flatbuffers_wrap.h
@@ -0,0 +1,10 @@
+// flatbuffers_wrap.h
+// Suppresses C++14 warnings
+#pragma once
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+
+#include <flatbuffers/flatbuffers.h>
+
+#pragma GCC diagnostic pop
